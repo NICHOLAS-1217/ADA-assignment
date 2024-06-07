b56924b124364235eddce5a892cf997a918a84f9
NICHOLAS-1217 <nicholastan1217@gmail.com>
2024-06-07 16:42:39 +0800

    kruskal algorithm done

--- MinimumSpanningTree.py
+++ MinimumSpanningTree.py
@@ -1,60 +1,96 @@
 from GraphDictionary import graph
 
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [0] * n
+class Graph: 
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [] 
+  
+    # Function to add an edge to graph 
+    def addEdge(self, u, v, w): 
+        self.graph.append([u, v, w]) 
+  
+    # A utility function to find set of an element i 
+    # (truly uses path compression technique) 
+    def find(self, parent, i): 
+        if parent[i] != i: 
+            # Reassignment of node's parent 
+            # to root node as 
+            # path compression requires 
+            parent[i] = self.find(parent, parent[i]) 
+        return parent[i] 
+  
+    # A function that does union of two sets of x and y 
+    # (uses union by rank) 
+    def union(self, parent, rank, x, y): 
+        # Attach smaller rank tree under root of 
+        # high rank tree (Union by Rank) 
+        if rank[x] < rank[y]: 
+            parent[x] = y 
+        elif rank[x] > rank[y]: 
+            parent[y] = x 
+        # If ranks are same, then make one as root 
+        # and increment its rank by one 
+        else: 
+            parent[y] = x 
+            rank[x] += 1
+  
+    # The main function to construct MST 
+    # using Kruskal's algorithm 
+    def KruskalMST(self, graph_dict): 
+        # This will store the resultant MST 
+        result = [] 
+  
+        # An index variable, used for sorted edges 
+        i = 0
+  
+        # An index variable, used for result[] 
+        e = 0
+  
+        # Sort all the edges in 
+        # non-decreasing order of their 
+        # weight 
+        edges = []
+        for u, neighbors in graph_dict.items():
+            for v, weight in neighbors.items():
+                edges.append((u, v, weight))
+        edges = sorted(edges, key=lambda item: item[2])
+  
+        parent = {} 
+        rank = {} 
+  
+        # Create V subsets with single elements 
+        for node in graph_dict.keys(): 
+            parent[node] = node
+            rank[node] = 0
+  
+        # Number of edges to be taken is less than to V-1 
+        while e < self.V - 1: 
+            # Pick the smallest edge and increment 
+            # the index for next iteration 
+            u, v, w = edges[i] 
+            i = i + 1
+            x = self.find(parent, u) 
+            y = self.find(parent, v) 
+  
+            # If including this edge doesn't 
+            # cause cycle, then include it in result 
+            # and increment the index of result 
+            # for next edge 
+            if x != y: 
+                e = e + 1
+                result.append([u, v, w]) 
+                self.union(parent, rank, x, y) 
+            # Else discard the edge 
+  
+        minimumCost = 0
+        print("Edges in the constructed MST") 
+        for u, v, weight in result: 
+            minimumCost += weight 
+            print("%s -- %s == %f" % (u, v, weight)) 
+        print("Minimum Spanning Tree weight:", minimumCost)
 
-    def find(self, u):
-        if self.parent[u] != u:
-            self.parent[u] = self.find(self.parent[u])
-        return self.parent[u]
+# Create an instance of Graph with the number of vertices
+g = Graph(len(graph))
 
-    def union(self, u, v):
-        root_u = self.find(u)
-        root_v = self.find(v)
-
-        if root_u != root_v:
-            if self.rank[root_u] > self.rank[root_v]:
-                self.parent[root_v] = root_u
-            elif self.rank[root_u] < self.rank[root_v]:
-                self.parent[root_u] = root_v
-            else:
-                self.parent[root_v] = root_u
-                self.rank[root_u] += 1
-
-def kruskal(graph):
-    # Convert the graph into a list of edges with weights
-    edges = []
-    nodes = list(graph.keys())
-    node_index = {node: i for i, node in enumerate(nodes)}
-
-    for node in graph:
-        for neighbor, weight in graph[node].items():
-            edges.append((weight, node, neighbor))
-
-    # Sort the edges based on their weights
-    edges.sort()
-
-    uf = UnionFind(len(nodes))
-
-    mst = []
-    total_weight = 0
-
-    for weight, u, v in edges:
-        u_index = node_index[u]
-        v_index = node_index[v]
-
-        if uf.find(u_index) != uf.find(v_index):
-            uf.union(u_index, v_index)
-            mst.append((u, v, weight))
-            total_weight += weight
-
-    return mst, total_weight
-
-# Execute Kruskal's algorithm
-mst, total_weight = kruskal(graph)
-print("Minimum Spanning Tree:")
-for edge in mst:
-    print(edge)
-print(f"Total weight: {total_weight}")
+# Call KruskalMST with the graph dictionary
+g.KruskalMST(graph)
